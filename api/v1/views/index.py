8b7f44cfcb8089cb0886821064d15c8e6416dfd5
davykoch <koechdavis24@gmail.com>
2024-04-03 02:02:25 +0300

    Api Status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,10 @@ from flask import Jsonify
 @app_views.route('/status', methods=[GET])
 def index():
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=[GET])
+def retrieve_stats():
+    classes_count = {}
+    for cls in storage.classes.values():
+        classes_count[cls.__name__] = storage.count(cls)
+    return jsonify(classes_count)
